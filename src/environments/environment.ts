14c7faed213681fd832f030246794c008a11ebc8
yarden_itzhaky <yardene015@gmail.com>
2024-12-30 13:21:01 +0200

    deleted api key

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  useEmulators: true, // Set to false when deploying to production
+  firebase: {
+    apiKey: "AIzaSyBA8xvn6CPX3RQF40Zbc874sucboFDB-7Q",
+    authDomain: "yooz-app-fc6cb.firebaseapp.com",
+    projectId: "yooz-app-fc6cb",
+    storageBucket: "yooz-app-fc6cb.firebasestorage.app",
+    messagingSenderId: "203612095839",
+    appId: "1:203612095839:web:f0e3e2fe857978de66e2ea",
+    measurementId: "G-82STNQXDVX"
+  }
+};
